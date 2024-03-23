517d6ec8252e94d33abccc8c9be27ef146c15110
[olegaches] <zelyoniyoleg@yandex.ru>
2024-03-23 09:01:26 +0500

    ok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.10"
+        kotlinCompilerExtensionVersion = "1.5.11"
     }
     packaging {
         resources {
